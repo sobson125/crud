7df04a7f687ee586416082cf18321f7c34b92e95
Grzegorz <grzegorz.sobczyk125@gmail.com>
2020-05-27 16:41:29 +0200

    Fix layout of packages

--- src/main/java/com/example/testapp/Products/ProductController.java
+++ src/main/java/com/example/testapp/Products/ProductController.java
@@ -1,10 +1,11 @@
-package com.example.testapp;
+package com.example.testapp.Products;
 
-import com.example.testapp.Products.Product;
-import com.example.testapp.Products.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
